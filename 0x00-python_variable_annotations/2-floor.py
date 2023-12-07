853492ddfe9958321239cca5935de6ed7b9f5c9f
Viestar <m.silverbuckson@gmail.com>
2023-12-07 19:51:16 +0300

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A  type-annotated function floor which takes a float floorNumber as
+argument and returns the floor of the float.
+"""
+import math
+
+
+def floor(floorNumber: float) -> int:
+    """ Returns a down rounded figure of a float """
+    return math.floor(floorNumber)
